b769a145ef9b30abebd382dc0b5e293b13ab2bfc
marcos contreras <5marcoscontreras5@gmail.com>
2025-10-23 10:59:42 +0200

    first commit

---
+++ src/git_colaboracion/git2025/HellowGit.java
@@ -0,0 +1,10 @@
+package git_colaboracion.git2025;
+
+public class HellowGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hellow Git");
+		System.out.println("Marcos Contreras Carrasquilla");
+	}
+
+}
