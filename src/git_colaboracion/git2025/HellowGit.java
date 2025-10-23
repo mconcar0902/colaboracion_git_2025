d5107f1eed79299853986d53d1f7f09afa339cee
Cristian Muñoz Díaz <munozdiazcristian1@gmail.com>
2025-10-23 11:18:15 +0200

    fix commit

--- src/git_colaboracion/git2025/HellowGit.java
+++ src/git_colaboracion/git2025/HellowGit.java
@@ -5,6 +5,7 @@ public class HellowGit {
 	public static void main(String[] args) {
 		System.out.println("Hellow Git");
 		System.out.println("Marcos Contreras Carrasquilla");
+		System.out.println("Cristian Muñoz Díaz");
 	}
 
 }
