eedb7cd51427226ff94df17768f18b7a76e9a2fb
manologomez123 <145774876+manologomez123@users.noreply.github.com>
2025-10-27 13:43:54 +0100

    Cambios realizados por el profesor

--- src/git_colaboracion/git2025/HellowGit.java
+++ src/git_colaboracion/git2025/HellowGit.java
@@ -7,6 +7,7 @@ public class HellowGit {
 		System.out.println("Marcos Contreras Carrasquilla");
 		System.out.println("Cristian Muñoz Díaz");
 		System.out.println("Jesus Zambrano Montero");
+		System.out.println("Manuel Gómez Henares");
 	}
 
 }
